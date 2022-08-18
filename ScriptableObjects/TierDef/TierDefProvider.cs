06df3c572f8de0ee5dd09e6737de632996e72efd
Bubbet <brett1057228@gmail.com>
2022-08-18 13:31:56 -0600

    Fix colors?

--- ScriptableObjects/TierDef/TierDefProvider.cs
+++ ScriptableObjects/TierDef/TierDefProvider.cs
@@ -9,59 +9,56 @@ using UnityEngine;
 
 namespace StormSurge.ScriptableObjects.TierDef
 {
-    [HarmonyPatch]
-    public class TierDefProvider
-    {
-        private static ItemTierDef _hunter;
-        public static ItemTierDef HunterTierDef
-        {
-            get
-            {
-                if (!_hunter) _hunter = Assets.AssetBundle.LoadAsset<ItemTierDef>("HunterTier");
-                return _hunter;
-            }
-        }
-        public static void Init(StormsurgeContentPack contentPack)
-        {
-            
-        }
+	[HarmonyPatch]
+	public class TierDefProvider
+	{
+		private static ItemTierDef _hunter;
 
-        [HarmonyPostfix, HarmonyPatch(typeof(ColorCatalog), MethodType.StaticConstructor)]
-        public static void AddNewColor()
-        {
-            var len = ColorCatalog.indexToColor32.Length;
-            HunterTierDef.colorIndex = (ColorCatalog.ColorIndex) len;
-            HunterTierDef.darkColorIndex = (ColorCatalog.ColorIndex) len + 1;
+		public static ItemTierDef HunterTierDef
+		{
+			get
+			{
+				if (!_hunter) _hunter = Assets.AssetBundle.LoadAsset<ItemTierDef>("HunterTier");
+				return _hunter;
+			}
+		}
 
-            var hunterLight = new Color32(223, 205, 255, 255);
-            var hunterDark = new Color32(88, 50, 86, 255);
-            
-            ColorCatalog.indexToColor32 = ColorCatalog.indexToColor32.AddItem(hunterLight).AddItem(hunterDark).ToArray();
-            ColorCatalog.indexToHexString = ColorCatalog.indexToHexString.AddItem(Util.RGBToHex(hunterLight)).AddItem(Util.RGBToHex(hunterDark)).ToArray();
-        }
+		public static void Init(StormsurgeContentPack contentPack)
+		{
+		}
 
-        [HarmonyPrefix, HarmonyPatch(typeof(ColorCatalog), nameof(ColorCatalog.GetColor))]
-        public static bool ReplaceGetColor(ColorCatalog.ColorIndex colorIndex, ref Color32 __result)
-        {
-            if (colorIndex < ColorCatalog.ColorIndex.None || (int) colorIndex > ColorCatalog.indexToColor32.Length)
-            {
-                colorIndex = ColorCatalog.ColorIndex.Error;
-            }
+		[HarmonyPostfix, HarmonyPatch(typeof(ColorCatalog), MethodType.StaticConstructor)]
+		public static void AddNewColor()
+		{
+			var len = ColorCatalog.indexToColor32.Length;
+			HunterTierDef.colorIndex = (ColorCatalog.ColorIndex) len;
+			HunterTierDef.darkColorIndex = (ColorCatalog.ColorIndex) len + 1;
 
-            __result = ColorCatalog.indexToColor32[(int) colorIndex];
-            return false;
-        }
-        
-        [HarmonyPrefix, HarmonyPatch(typeof(ColorCatalog), nameof(ColorCatalog.GetColorHexString))]
-        public static bool ReplaceGetHexString(ColorCatalog.ColorIndex colorIndex, ref string __result)
-        {
-            if (colorIndex < ColorCatalog.ColorIndex.None || (int) colorIndex > ColorCatalog.indexToHexString.Length)
-            {
-                colorIndex = ColorCatalog.ColorIndex.Error;
-            }
+			var hunterLight = new Color32(223, 205, 255, 255);
+			var hunterDark = new Color32(88, 50, 86, 255);
 
-            __result = ColorCatalog.indexToHexString[(int) colorIndex];
-            return false;
-        }
-    }
+			ColorCatalog.indexToColor32 =
+				ColorCatalog.indexToColor32.AddItem(hunterLight).AddItem(hunterDark).ToArray();
+			ColorCatalog.indexToHexString = ColorCatalog.indexToHexString.AddItem(Util.RGBToHex(hunterLight))
+				.AddItem(Util.RGBToHex(hunterDark)).ToArray();
+		}
+
+		[HarmonyPrefix, HarmonyPatch(typeof(ColorCatalog), nameof(ColorCatalog.GetColor))]
+		public static bool PatchGetColor(ColorCatalog.ColorIndex colorIndex, ref Color32 __result)
+		{
+			var ind = (int) colorIndex;
+			if (ind >= ColorCatalog.indexToColor32.Length) return true;
+			__result = ColorCatalog.indexToColor32[ind];
+			return false;
+		}
+
+		[HarmonyPrefix, HarmonyPatch(typeof(ColorCatalog), nameof(ColorCatalog.GetColorHexString))]
+		public static bool GetColorHexString(ColorCatalog.ColorIndex colorIndex, ref string __result)
+		{
+			var ind = (int) colorIndex;
+			if (ind >= ColorCatalog.indexToHexString.Length) return true;
+			__result = ColorCatalog.indexToHexString[ind];
+			return false;
+		}
+	}
 }
